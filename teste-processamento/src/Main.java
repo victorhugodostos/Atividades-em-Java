1b4b629c0cc1b8aae3b05def860dc82760149ce9
victorhugodostos <54106190+victorhugodostos@users.noreply.github.com>
2020-12-16 13:09:52 -0300

    Add files via upload

---
+++ teste-processamento/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double x;
+		x = sc.nextDouble();
+		System.out.println("Você digitou: " + x);
+		
+		
+		
+		sc.close();
+	}
+	 
+}
